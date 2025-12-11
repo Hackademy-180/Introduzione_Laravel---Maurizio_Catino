7556f7043cd686a67f016ca82c0945fdcd00e081
Maurizio Catino <mauriziocatino96@gmail.com>
2025-12-11 18:42:33 +0100

    Aggiunti elementi estetici

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,29 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   </head>
   <body>
-
+    <nav class="navbar navbar-expand-lg bg-info border border border-black rounded-5">
+        <div class="container-fluid">
+                <a class="navbar-brand" href="#">Homepage</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Laravel</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">Project</a>
+                        </li>
+                        <li class="nav-item dropdown">     
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                    
+                </form>
+            </div>
+        </div>
+    </nav>
     <h1>Questo è il nuovo progetto Laravel!</h1>
     @if($nome)
         <p>Benvenuto {{$nome}}</p>
